68c486189cd0df75ef8b6ea1d1d3be3a6f0b81df
Buster Major <Buster.Major@red-gate.com>
2023-03-06 11:50:45 +0000

    init

---
+++ BerlinClock/BerlinClock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace BerlinClock
+{
+    public class BerlinClock
+    {
+        private readonly DateTime _date;
+
+        public BerlinClock(DateTime date)
+        {
+            _date = date;
+        }
+
+        public string GetSingleMinutes()
+        {
+            var numMinutes = _date.Minute % 5;
+            return $"{new String('Y', numMinutes)}{new String('O', 4 - numMinutes)}";
+        }
+
+        public string GetFiveMinutes()
+        {
+            var numMinutes = _date.Minute / 5;
+            return $"{string.Concat(Enumerable.Repeat("YYR", numMinutes / 3))}" +
+                   $"{new String('Y', numMinutes % 3)}" +
+                   $"{new String('O', 11 - numMinutes)}";
+        }
+    }
+}
---
+++ BerlinClockTests/Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BerlinClockTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase("01/01/2000 00:00:00", "OOOO")]
+        [TestCase("01/01/2000 23:59:59", "YYYY")]
+        [TestCase("01/01/2000 12:32:00", "YYOO")]
+        [TestCase("01/01/2000 12:34:00", "YYYY")]
+        [TestCase("01/01/2000 12:35:00", "OOOO")]
+        public void SingleMinutesRow(string dateTime, string expected)
+        {
+            var berlinClock = new BerlinClock.BerlinClock(DateTime.Parse(dateTime));
+            Assert.AreEqual(expected, berlinClock.GetSingleMinutes());
+        }
+
+        [TestCase("01/01/2000 00:00:00", "OOOOOOOOOOO")]
+        [TestCase("01/01/2000 23:59:59", "YYRYYRYYRYY")]
+        [TestCase("01/01/2000 12:04:00", "OOOOOOOOOOO")]
+        [TestCase("01/01/2000 12:23:00", "YYRYOOOOOOO")]
+        [TestCase("01/01/2000 12:35:00", "YYRYYRYOOOO")]
+        public void FiveMinutes(string dateTime, string expected)
+        {
+            var berlinClock = new BerlinClock.BerlinClock(DateTime.Parse(dateTime));
+            Assert.AreEqual(expected, berlinClock.GetFiveMinutes());
+        }
+    }
+}
